59bc81dcda9d52011f2bf1cd6a820f99cf3592f0
Saim <saimh355@gmail.com>
2020-01-16 10:44:04 +0500

    Kotlin Quiz Application

--- app/src/main/java/com/example/quizkotlin/MainActivity.kt
+++ app/src/main/java/com/example/quizkotlin/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.quizkotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.GridLayout
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.quizkotlin.Adapter.CategoryAdapter
+import com.example.quizkotlin.Common.SpaceItemDecoration
+import com.example.quizkotlin.DBHelper.DBHelper
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        toolbar.title = "Saim Quiz 2020"
+        setSupportActionBar(toolbar)
+        recycler_category.setHasFixedSize(true)
+        recycler_category.layoutManager = GridLayoutManager(this,2)
+
+        val adapter = CategoryAdapter(this,DBHelper.getInstance(this).allCategories)
+        recycler_category.addItemDecoration(SpaceItemDecoration(4))
+        recycler_category.adapter = adapter
     }
 }
